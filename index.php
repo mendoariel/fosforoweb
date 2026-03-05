1d4522dccaa2143a5c3ccf32f978033b59472fbd
mendoariel <mendoariel@hotmail.com>
2026-03-05 09:11:57 -0300

    Update favicon with new logo

--- index.php
+++ index.php
@@ -59,12 +59,12 @@ try {
     <meta property="twitter:image" content="https://fosforoweb.com.ar/assets/images/logo-main.png">
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="favicon.ico?v=2">
-    <link rel="icon" type="image/png" sizes="1024x1024" href="favicon.png?v=3">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon.png?v=3">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon.png?v=3">
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon.png?v=3">
-    <link rel="shortcut icon" href="favicon.ico?v=2">
+    <link rel="icon" type="image/x-icon" href="favicon.ico?v=3">
+    <link rel="icon" type="image/png" sizes="1024x1024" href="favicon.png?v=4">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon.png?v=4">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon.png?v=4">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon.png?v=4">
+    <link rel="shortcut icon" href="favicon.ico?v=3">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
